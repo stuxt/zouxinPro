0284613bc48f97bff7b5cd82e447fc2abdb6ba64
小天同学 <yajun.chen312@gmail.com>
2017-06-24 13:28:38 +0800

    init

---
+++ zouxinProData.js
@@ -0,0 +1,20 @@
+var zouxinProData = [{
+  id:2,
+  imgUrl:"http://tupian.aladd.net/2017/6/883.jpg",
+  date:"2017-06-26 星期一",
+  verInfo:"Vol.002"
+  content:"我是测试内容，我很文艺而且很优雅富有哲理。3",
+  bookName:"我的世界，我做主1",
+  imgFrom:"aladd",
+  author:"小天同学"
+},
+{
+  id:1,
+  imgUrl:"http://tupian.aladd.net/2017/6/1334.jpg",
+  date:"2017-06-23 星期五",
+  verInfo:"Vol.001"
+  content:"我是测试内容，我很文艺而且很优雅富有哲理。",
+  bookName:"我的世界，我做主",
+  imgFrom:"aladd",
+  author:"小天同学"
+}]
